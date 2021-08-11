f37d5ec6b7842f1b8c9e7303d6a341a8c810a10a
Rafal-1999 <skapinka1999@wp.pl>
2021-08-11 12:40:21 +0200

    Assigning elements to variables

---
+++ js/currency-exchange.js
@@ -0,0 +1,7 @@
+let firstCurrency = document.querySelector(".js-firstCurrency");
+let amount = document.querySelector(".js-amount");
+let exchangeButton = document.querySelector(".js-exchangeButton");
+let currentRate = document.querySelector(".js-currentRate");
+let secondCurrency = document.querySelector(".js-secondCurrency");
+let result = document.querySelector(".js-result");
+let convertButton = document.querySelector(".js-convertButton");
